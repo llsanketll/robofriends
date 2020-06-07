2a8ad461928148cf705ef013bc0aff7555164f84
llsanketll <sanket.lamsal@gmail.com>
2020-06-07 22:49:44 +0545

    Updates

---
+++ precache-manifest.35fc115e48c8d5443f96710f32b23c6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5c0c83b91975c80d17730e93de54fa",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "f86b062d2d7916dcf1e0",
+    "url": "/robofriends/static/css/main.15128998.chunk.css"
+  },
+  {
+    "revision": "e6274bb51b25f098d5e8",
+    "url": "/robofriends/static/js/2.0d983b4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0d983b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86b062d2d7916dcf1e0",
+    "url": "/robofriends/static/js/main.8fe2a6f2.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
